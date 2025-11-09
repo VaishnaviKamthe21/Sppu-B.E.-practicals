2a15c612b67d873328afdecba4bf4b9fbbc7b44f
VaishnaviKamthe21 <vaishnavikamthe1884@gmail.com>
2025-11-09 17:47:52 +0530

    added fibonacci prac1

---
+++ Fibonacci.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Fibonacci{
+    
+    void fibo_itter(int n){
+        int sum = 0, steps = 0;
+        if(n<0){
+            sum+=-1;
+            steps++;
+        }
+        if(n==1 || n==0){
+            sum+=n;
+            steps++;
+        }
+
+        int a=0, b=1;
+
+        for(int i=2;i<=n;i++){
+            sum = a+b;
+            a = b;
+            b = sum;
+
+            steps++;
+        }
+
+        System.out.println("Sum = "+sum);
+        System.out.println("Steps = "+steps);
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length: ");
+        int n = sc.nextInt();
+        Fibonacci f = new Fibonacci();
+        f.fibo_itter(n);
+    }
+}
